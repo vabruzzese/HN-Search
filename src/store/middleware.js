a154afbeaae46d6149c390a803f4f065fa78497c
Vince Abruzzese <vince.m.abruzzese@gmail.com>
2020-03-08 11:14:10 -0400

    remove thunk

--- src/store/middleware.js
+++ src/store/middleware.js
@@ -1,11 +1,8 @@
 import { applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 
 const isProd = process.env.NODE_ENV === 'production';
 const middlewareList = [];
-
-middlewareList.push(thunk);
 
 if (!isProd) {
     middlewareList.push(createLogger());
